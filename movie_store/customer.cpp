d4bb26615da644ff17d6813320b1540622744db9
LoaiMohd <loaimohdzakaria216@gmail.com>
2025-04-13 11:54:09 +0200

    implemented generateId function

--- movie_store/customer.cpp
+++ movie_store/customer.cpp
@@ -1,8 +1,13 @@
 #include "customer.h"
 
-
-int generateId(){
+std::string generateId(){
+    std::string Id = "C#";
+    srand(time(0)); // changes seed
+    for (int i =1; i<=10; i++){
+        Id+= std::to_string(rand()%10); // generates number from 0 -> 9 and adds it
+    }
     
+    return Id;
 }
 
 void addNewCustomer(){
--- movie_store/customer.h
+++ movie_store/customer.h
@@ -3,11 +3,10 @@
 #define limit 8
 
 struct Customer{
-    std::string Name, PhoneNumber;
-    int Id;
+    std::string Name, PhoneNumber, Id;
     std::string CurrentlyRentedMovies[limit];
     std::vector<std::string> PreviouslyRentedMovies;
 };
 
-int generateId();
+std::string generateId();
 void addNewCustomer(); // TODO: takes an array of Customer
