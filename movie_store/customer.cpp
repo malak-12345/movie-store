eb34ff8cd46ba6ca9999db3f244c76d16d062273
LoaiMohd <loaimohdzakaria216@gmail.com>
2025-04-14 08:02:51 +0200

    implemented checkphonenumber function

--- movie_store/customer.cpp
+++ movie_store/customer.cpp
@@ -1,14 +1,67 @@
 #include "customer.h"
+#include <iostream>
+#include <algorithm>
 
-std::string generateId(){
+std::string generateId()
+{
     std::string Id = "C#";
     srand(time(0)); // changes seed
-    for (int i =1; i<=10; i++){
+    for (int i =1; i<=4; i++)
+    {
         Id+= std::to_string(rand()%10); // generates number from 0 -> 9 and adds it
     }
-    
     return Id;
 }
 
-void addNewCustomer(){
+std::string deleteSpaces(std::string str)
+{
+    str.erase(remove(str.begin(),str.end(), ' '), str.end());
+    return str;
+}
+
+bool checkPhoneNumber(std::string PhoneNumber)
+{
+    char digits[10] = {'0','1','2','3','4','5','6','7','8','9'};
+
+    PhoneNumber = deleteSpaces(PhoneNumber);
+    // std::cout << PhoneNumber << '\n';
+    // std::cout << PhoneNumber.length() << '\n';
+
+    if(PhoneNumber.length()>=5 && PhoneNumber.length()<=15)
+    {
+        for(int i=0; i<PhoneNumber.length(); i++)
+        {
+            bool isDigit = false;
+            
+            // std::cout << "Outer loop:" << PhoneNumber[i] << '\n';
+            // std::cout << sizeof(PhoneNumber[i]) << '\n';
+            
+            for(int j=0; j<10; j++)
+            {
+                // std::cout << "i: " << i << "j: " << j << '\n';
+                if(PhoneNumber[i] == digits[j])
+                {
+                    // std::cout << "Inner loop:" << digits[j] << '\n';
+                    isDigit = true;
+                    break;
+                }
+            }
+        
+            if(!isDigit)
+            {
+                // std::cout << "not a digit\n";
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+  
+}
+
+void addNewCustomer()
+{
+    
 }
--- movie_store/customer.h
+++ movie_store/customer.h
@@ -2,11 +2,14 @@
 #include <vector>
 #define limit 8
 
-struct Customer{
+struct Customer
+{
     std::string Name, PhoneNumber, Id;
     std::string CurrentlyRentedMovies[limit];
     std::vector<std::string> PreviouslyRentedMovies;
 };
 
 std::string generateId();
+std::string deleteSpaces(std::string str);
+bool checkPhoneNumber(std::string PhoneNumber);
 void addNewCustomer(); // TODO: takes an array of Customer
