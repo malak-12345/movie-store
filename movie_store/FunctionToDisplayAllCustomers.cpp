50b7a57259486fe434dc8ceca0a940f15203beff
LoaiMohd <loaimohdzakaria216@gmail.com>
2025-04-15 16:48:33 +0200

    Update FunctionToDisplayAllCustomers.cpp

--- movie_store/FunctionToDisplayAllCustomers.cpp
+++ movie_store/FunctionToDisplayAllCustomers.cpp
@@ -1,4 +1,6 @@
-Customer customers[];
+// Customer customers[];
+Customer customers[10] = {{"name1", "11111111111","1234", {"movie1","movie2","movie3"}}, {"name2","22222222222", "5678", {"movie1","movie2","movie3"}}};
+
 void list(Customer customers[], int number_of_customers)
 {
     for (int i = 0;i < number_of_customers;i++)
